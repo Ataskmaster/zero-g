24b1af9b7256299f95cefad36294342ff2fbf687
Siddharth Garg <siddharth.garg@BRILLIO.COM>
2020-03-11 16:34:30 +0530

    Subscription page without fuse.js

--- src/containers/Subscribe/Subscribe.js
+++ src/containers/Subscribe/Subscribe.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   Grommet,
   Table,
@@ -10,99 +10,120 @@ import {
   Button,
   Box
 } from "grommet";
-import Fuse from "fuse.js";
-// import styled from "styled-components";
 import theme from "../../utils/theme";
 
 export const Subscribe = () => {
-  const [list] = useState([
-    {
-      id: 1,
-      projectId: "RNH",
-      projectName: "Realtor New Homes"
-    },
-    {
-      id: 2,
-      projectId: "VW",
-      projectName: "Verizon Wireless"
-    },
-    {
-      id: 3,
-      projectId: "MCP",
-      projectName: "Mastercard Cashless Payments"
-    }
-  ]);
+  const [projects, setProjects] = useState([]);
 
-  const [user, setUser] = useState([2, 3]);
+  const [subscriptions, setSubscriptions] = useState([]);
 
-  let dupUser = [...user];
+  useEffect(() => {
+    setProjects([
+      {
+        id: 1,
+        projectId: "RNH",
+        projectName: "Realtor New Homes"
+      },
+      {
+        id: 2,
+        projectId: "VW",
+        projectName: "Verizon Wireless"
+      },
+      {
+        id: 3,
+        projectId: "MCP",
+        projectName: "Mastercard Cashless Payments"
+      },
+      {
+        id: 4,
+        projectId: "ZG",
+        projectName: "Zero-G"
+      }
+    ]);
+    setSubscriptions([1, 3]);
+  }, []);
 
   const [query, setQuery] = useState("");
 
-  const options = {
-    shouldSort: true,
-    threshold: 0.2,
-    maxPatternLength: 32,
-    minMatchCharLength: 1,
-    keys: ["projectId", "projectName"]
-  };
-  let fuse = new Fuse(list, options);
-
-  const data = query ? fuse.search(query) : list;
+  let dupSubscriptions = [...subscriptions];
+  let data = [...projects];
+  let search = query.trim().toLowerCase();
+  if (search.length > 0) {
+    search = search.replace(/[-[\]{}()*+?.,\\^$|#\\s]/g, "\\$&");
+    data = data.filter(function(item) {
+      return (item.projectId + " " + item.projectName)
+        .toLowerCase()
+        .match(search);
+    });
+  }
 
   return (
     <Grommet theme={theme}>
-      <Box justify="center" align="center" alignContent="center">
-        <TextInput
-          placeholder="start typing your project name"
-          onChange={event => setQuery(event.target.value)}
-        />
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableCell scope="col" border="bottom">
-                Project ID
-              </TableCell>
-              <TableCell scope="col" border="bottom">
-                Project Name
-              </TableCell>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {data.map((item, _index) => {
-              return (
-                <TableRow key={item.id}>
-                  <TableCell scope="row">
-                    <strong>{item.projectId}</strong>
-                  </TableCell>
-                  <TableCell>{item.projectName}</TableCell>
-                  <TableCell>
-                    <Button
-                      label={
-                        dupUser.indexOf(item.id) === -1
-                          ? "Subscribe"
-                          : "Unsubscribe"
-                      }
-                      value={item.id}
-                      color="#55CCCC"
-                      onClick={
-                        dupUser.indexOf(item.id) === -1
-                          ? () => {
-                              dupUser.push(item.id);
-                              setUser(dupUser);
-                            }
-                          : () => {
-                              dupUser.splice(dupUser.indexOf(item.id), 1);
-                              setUser(dupUser);
-                            }
-                      }
-                    />
-                  </TableCell>
-                </TableRow>
-              );
-            })}
-          </TableBody>
-        </Table>
+      <Box justify="center" align="center" alignContent="center" gap="medium">
+        <Box width="xlarge">
+          <TextInput
+            placeholder="start typing your project name"
+            onChange={event => setQuery(event.target.value)}
+            size="medium"
+            autoFocus
+          />
+        </Box>
+        <Box>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableCell scope="col" border="bottom">
+                  Project ID
+                </TableCell>
+                <TableCell scope="col" border="bottom">
+                  Project Name
+                </TableCell>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {data.map((item, _index) => {
+                return (
+                  <TableRow key={item.id}>
+                    <TableCell scope="row">
+                      <strong>{item.projectId}</strong>
+                    </TableCell>
+                    <TableCell>{item.projectName}</TableCell>
+                    <TableCell>
+                      <Button
+                        label={
+                          dupSubscriptions.indexOf(item.id) === -1
+                            ? "Subscribe"
+                            : "Unsubscribe"
+                        }
+                        value={item.id}
+                        primary
+                        color={
+                          dupSubscriptions.indexOf(item.id) === -1
+                            ? "#03fc98"
+                            : "#fcba03"
+                        }
+                        onClick={
+                          dupSubscriptions.indexOf(item.id) === -1
+                            ? () => {
+                                dupSubscriptions.push(item.id);
+                                setSubscriptions(dupSubscriptions);
+                              }
+                            : () => {
+                                dupSubscriptions.splice(
+                                  dupSubscriptions.indexOf(item.id),
+                                  1
+                                );
+                                setSubscriptions(dupSubscriptions);
+                              }
+                        }
+                      />
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </Box>
       </Box>
     </Grommet>
   );
